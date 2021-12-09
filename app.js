c0fef4258d06cac68a3bb78ce7490a72c7b9b350
beekaryss <91239982+beekaryss@users.noreply.github.com>
2021-12-10 04:40:59 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+  const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+var mySong = document.getElementById("mySong");
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+  if(mySong.paused){
+  mySong.play();
+  icon.src = "pause.png";
+}else{
+  mySong.pause();
+  icon.src = "play.png"
+}
+
+}
